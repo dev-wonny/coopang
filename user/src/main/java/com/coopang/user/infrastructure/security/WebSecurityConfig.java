d875d903f27905922bd5a6cd68a74d197e76c08b
“devwonny” <devwonny@gmail.com>
2024-09-14 20:07:14 +0900

    [Update] user server - spring security 로그인 설정 일시 막기
    [Update] user server - spring security 로그인 설정 일시 막기
    그라파나 모니터링 세팅을 하는데 있어서, spring security login 이 뜨지 않도록 처리함

---
+++ user/src/main/java/com/coopang/user/infrastructure/security/WebSecurityConfig.java
@@ -0,0 +1,25 @@
+package com.coopang.user.infrastructure.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity // Spring Security 지원을 가능하게 함
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // CSRF 설정
+        http.csrf((csrf) -> csrf.disable());
+
+        http.authorizeHttpRequests((authorizeHttpRequests) ->
+                authorizeHttpRequests
+                        .anyRequest()
+                        .permitAll());
+
+        return http.build();
+    }
+}
