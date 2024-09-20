918d98bcfe427d2788e44f24c140bd0f20fce1fd
“devwonny” <devwonny@gmail.com>
2024-09-20 14:18:13 +0900

    [Add] user server: add cache
    [Add] user server: add cache
    Add spring-boot-starter-cache, caffeine
    Adjust @EnableCaching, @Cacheable, @CacheEvict

--- user/src/main/java/com/coopang/user/UserApplication.java
+++ user/src/main/java/com/coopang/user/UserApplication.java
@@ -2,9 +2,11 @@ package com.coopang.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
+@EnableCaching
 @EnableJpaAuditing
 @SpringBootApplication
 @EnableMethodSecurity(securedEnabled = true)
