2f6005df41d6ad84dc92397c040e333136c7d08a
“devwonny” <devwonny@gmail.com>
2024-10-01 09:00:19 +0900

    [Init] api-communication
    [Init] api-communication
    
    - Clients for communicating with external systems, Feign, Kafka, etc.

---
+++ api-communication/src/test/java/com/coopang/api_communication/ApiCommunicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coopang.api_communication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCommunicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
