b22a87e65674bec497e067e9e9388f3ca591fa90
bongdaehyun <44823780+bongdaehyun@users.noreply.github.com>
2024-10-02 15:46:21 +0900

    feature : 카테고리 테스트 데이터 자동으로 생성  #30

--- BOOT/product/src/main/java/com/coopang/product/domain/repository/CategoryRepository.java
+++ BOOT/product/src/main/java/com/coopang/product/domain/repository/CategoryRepository.java
@@ -1,9 +1,11 @@
 package com.coopang.product.domain.repository;
 
 import com.coopang.product.domain.entitiy.CategoryEntity;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface CategoryRepository {
     Optional<CategoryEntity> findById(UUID categoryId);
+
 }
