a7f42871311b7a12c603aaaf64829f6f72751170
“devwonny” <devwonny@gmail.com>
2024-10-03 19:46:20 +0900

    [Update] SecurityFilterProperties : 개선
    [Update] SecurityFilterProperties : 개선
    - Component 제거
    - setter 제거
    - https://cutewonny.tistory.com/entry/Component-Configuration 에 설명

--- INFRA/api-config/src/main/java/com/coopang/apiconfig/security/config/SecurityFilterProperties.java
+++ INFRA/api-config/src/main/java/com/coopang/apiconfig/security/config/SecurityFilterProperties.java
@@ -1,21 +1,16 @@
 package com.coopang.apiconfig.security.config;
 
+import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Component
+@Getter
 @ConfigurationProperties(prefix = "common.api.filter")
 public class SecurityFilterProperties {
-
     private List<String> paths;
 
-    public List<String> getPaths() {
-        return paths;
-    }
-
-    public void setPaths(List<String> paths) {
+    public SecurityFilterProperties(List<String> paths) {
         this.paths = paths;
     }
 }
