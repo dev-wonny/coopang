f3f01b4bcbce0e592bf53d0aa8faf66e4b0fea9a
bongdaehyun <44823780+bongdaehyun@users.noreply.github.com>
2024-10-04 01:01:33 +0900

    update : 상품 재고관련 타입 변경 #30

---
+++ BOOT/product/src/main/java/com/coopang/product/domain/entitiy/vo/converter/ProductStockConverter.java
@@ -0,0 +1,19 @@
+package com.coopang.product.domain.entitiy.vo.converter;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Converter(autoApply = true)
+public class ProductStockConverter implements AttributeConverter<AtomicInteger, Integer>{
+
+    @Override
+    public Integer convertToDatabaseColumn(AtomicInteger attribute) {
+        return attribute.get();
+    }
+
+    @Override
+    public AtomicInteger convertToEntityAttribute(Integer dbData) {
+        return new AtomicInteger(dbData);
+    }
+}
