1c873b0efb70b7d3609b703e8d301ed269752876
bongdaehyun <44823780+bongdaehyun@users.noreply.github.com>
2024-10-04 16:15:02 +0900

    [Update] 상품 수량 0이상인 것들만 조회하도록 설정 #30

--- BOOT/product/src/main/java/com/coopang/product/domain/repository/ProductRepository.java
+++ BOOT/product/src/main/java/com/coopang/product/domain/repository/ProductRepository.java
@@ -1,26 +1,27 @@
 package com.coopang.product.domain.repository;
 
-import com.coopang.product.infrastructure.entity.ProductEntity;
+import com.coopang.product.domain.entity.ProductEntity;
 import com.coopang.product.presentation.request.ProductSearchCondition;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository {
 
-    @Query("SELECT p FROM ProductEntity p JOIN FETCH p.productStockEntity s JOIN FETCH p.categoryEntity c WHERE p.productId = :productId")
-    Optional<ProductEntity> findById(UUID productId);
+    Optional<ProductEntity> getOneByProductId(UUID productId);
 
     ProductEntity save(ProductEntity productEntity);
 
-    @Query("SELECT p FROM ProductEntity p JOIN FETCH p.productStockEntity s JOIN FETCH p.categoryEntity c")
+    @Query("SELECT p FROM ProductEntity p JOIN FETCH p.productStockEntity s JOIN FETCH p.categoryEntity c WHERE p.isDeleted = false and p.isHidden = false and p.isSale = true and s.productStock.value > 0")
     Page<ProductEntity> findAllWithStockAndCategory(Pageable pageable);
 
     Page<ProductEntity> search(ProductSearchCondition productSearchCondition, Pageable pageable);
 
-    @Query("SELECT p FROM ProductEntity p JOIN FETCH p.productStockEntity s JOIN FETCH p.categoryEntity c WHERE p.categoryEntity.categoryId = :categoryId" )
+    @Query("SELECT p FROM ProductEntity p JOIN FETCH p.productStockEntity s JOIN FETCH p.categoryEntity c "
+        + "WHERE p.categoryEntity.categoryId = :categoryId and p.isDeleted = false and p.isHidden = false and p.isSale = true and s.productStock.value > 0" )
     Page<ProductEntity> findAllWithStockAndCategoryByCategoryId(UUID categoryId,Pageable pageable);
 
 }
