ca360a4119f9b237abf030b7fca003479913758d
“devwonny” <devwonny@gmail.com>
2024-10-10 20:43:20 +0900

    [Update] DataInitializer for Seed data
    [Update] DataInitializer for Seed data
    - [Update] CompanyDataInitializer : Add HubMapInitializer
    - [Update] HubDataInitializer : initdata.HubRegions
    - [Add] ShipperDataInitializer

--- BOOT/hub/src/main/java/com/coopang/hub/infrastructure/configuration/CompanyDataInitializer.java
+++ BOOT/hub/src/main/java/com/coopang/hub/infrastructure/configuration/CompanyDataInitializer.java
@@ -1,5 +1,19 @@
 package com.coopang.hub.infrastructure.configuration;
 
+import static com.coopang.apiconfig.initdata.HubRegions.BUSAN;
+import static com.coopang.apiconfig.initdata.HubRegions.CHUNGBUK;
+import static com.coopang.apiconfig.initdata.HubRegions.DAEGU;
+import static com.coopang.apiconfig.initdata.HubRegions.DAEJEON;
+import static com.coopang.apiconfig.initdata.HubRegions.GANGWON;
+import static com.coopang.apiconfig.initdata.HubRegions.GWANGJU;
+import static com.coopang.apiconfig.initdata.HubRegions.GYEONGGI_NORTH;
+import static com.coopang.apiconfig.initdata.HubRegions.GYEONGGI_SOUTH;
+import static com.coopang.apiconfig.initdata.HubRegions.INCHEON;
+import static com.coopang.apiconfig.initdata.HubRegions.SEJONG;
+import static com.coopang.apiconfig.initdata.HubRegions.SEOUL;
+import static com.coopang.apiconfig.initdata.HubRegions.ULSAN;
+
+import com.coopang.apiconfig.initdata.HubMapInitializer;
 import com.coopang.hub.application.request.company.CompanyDto;
 import com.coopang.hub.application.service.company.CompanyService;
 import org.springframework.boot.CommandLineRunner;
@@ -12,10 +26,13 @@ import java.util.UUID;
 @Component
 public class CompanyDataInitializer implements CommandLineRunner {
     private final CompanyService companyService;
-    private int uuidIndex = 0;
+    private Map<String, UUID> hubMap;
+    private Map<String, UUID[]> companyManagers;
 
-    public CompanyDataInitializer(CompanyService companyService) {
+    public CompanyDataInitializer(CompanyService companyService, HubMapInitializer hubMapInitializer) {
         this.companyService = companyService;
+        this.hubMap = hubMapInitializer.getHubMap();
+        setCompanyManagers();
     }
 
     @Override
@@ -23,89 +40,64 @@ public class CompanyDataInitializer implements CommandLineRunner {
         createCompanies();
     }
 
-    // 허브 지역 정보 제공
-    private String[] getHubRegions() {
-        return new String[] {
-                "서울", "경기북부", "경기남부", "부산", "대구", "인천", "광주", "대전", "울산", "세종", "강원", "충북"
-        };
-    }
-
-    // 고정된 hub_id 배열
-    UUID[] hubIds = {
-            UUID.fromString("12fc1d66-4270-4468-b76d-93264856327b"),  // 서울
-            UUID.fromString("4dd1bc35-fecf-40a6-a3cd-281a6a800752"),  // 경기북부
-            UUID.fromString("83f3e5ab-adda-4d53-adb1-bf2e00f80af6"),  // 경기남부
-            UUID.fromString("83004b5d-122a-4bab-a27c-78824da0ad77"),  // 부산
-            UUID.fromString("d25e0314-0b9d-47dc-8a35-afd4381d3a9e"),  // 대구
-            UUID.fromString("094f9450-8916-4d5d-872f-af929cba79f7"),  // 인천
-            UUID.fromString("297e573f-a450-46c4-b0ac-3d318ae66e27"),  // 광주
-            UUID.fromString("f2bea779-3987-474b-a044-d707551ad689"),  // 대전
-            UUID.fromString("773db62c-eae9-4d4e-9029-d24f348f8c61"),  // 울산
-            UUID.fromString("c3aebe6d-a257-4798-b3ce-b9595c3c59f3"),  // 세종
-            UUID.fromString("666278fe-006f-4895-a094-bac433c2d669"),  // 강원
-            UUID.fromString("c5fb540b-63f0-464a-951c-6cd711986440")   // 충북
-    };
-
     // COMPANY 권한을 가진 user_id 매핑 (지역별)
-    private Map<String, UUID[]> companyManagers = new HashMap<>() {{
-        put("서울", new UUID[] {
+    private void setCompanyManagers() {
+        companyManagers = new HashMap<>();
+        companyManagers.put(SEOUL, new UUID[] {
                 UUID.fromString("4a89c46b-eaf3-49db-a0ad-862f87dc7fab"),
                 UUID.fromString("38f078b2-d4d4-404f-a4b8-d3f5b360e63b")
         });
-        put("경기북부", new UUID[] {
+        companyManagers.put(GYEONGGI_NORTH, new UUID[] {
                 UUID.fromString("2f86ccac-7800-4182-b6ec-9c489d515bfe"),
                 UUID.fromString("2c1cafc4-4085-4ebd-9b0e-872a5b6e2031")
         });
-        put("경기남부", new UUID[] {
+        companyManagers.put(GYEONGGI_SOUTH, new UUID[] {
                 UUID.fromString("a7104e8e-9834-4486-94f3-c0b2db10277d"),
                 UUID.fromString("746f8699-5753-4458-91c6-37937153cf8a")
         });
-        put("부산", new UUID[] {
+        companyManagers.put(BUSAN, new UUID[] {
                 UUID.fromString("118acc6b-954b-4df7-b1d8-2ecb10e62bf7"),
                 UUID.fromString("37f487ec-675c-48de-a653-cb0c1485adc6")
         });
-        put("대구", new UUID[] {
+        companyManagers.put(DAEGU, new UUID[] {
                 UUID.fromString("e9035f9e-924f-45e4-a461-b8e7e0ab651e"),
                 UUID.fromString("fac08a5f-1039-46bb-bc62-7487e0042aa5")
         });
-        put("인천", new UUID[] {
+        companyManagers.put(INCHEON, new UUID[] {
                 UUID.fromString("903d74e4-7c0d-436a-b259-9ebe1b9c2fb7"),
                 UUID.fromString("739586df-8dac-443a-be5a-cf4a10863c4a")
         });
-        put("광주", new UUID[] {
+        companyManagers.put(GWANGJU, new UUID[] {
                 UUID.fromString("538df806-4e21-4eec-8e65-8301c3c508d3"),
                 UUID.fromString("82cf3fdc-ab02-40ca-960f-bd6df52c85f4")
         });
-        put("대전", new UUID[] {
+        companyManagers.put(DAEJEON, new UUID[] {
                 UUID.fromString("57f7d9fd-1b80-4647-bb07-6fc454620647"),
                 UUID.fromString("1827468a-5522-417f-9979-34ecae003e10")
         });
-        put("울산", new UUID[] {
+        companyManagers.put(ULSAN, new UUID[] {
                 UUID.fromString("17c1ba04-e7db-4f78-a8df-cfbd02582d0c"),
                 UUID.fromString("814332a7-54ba-4640-bc65-6cec63c5b96b")
         });
-        put("세종", new UUID[] {
+        companyManagers.put(SEJONG, new UUID[] {
                 UUID.fromString("15ea2d34-1d6b-4641-a014-7759e1e0062a"),
                 UUID.fromString("3bccf246-92f1-425b-9c9a-904c5458ea1d")
         });
-        put("강원", new UUID[] {
+        companyManagers.put(GANGWON, new UUID[] {
                 UUID.fromString("333698b2-26a0-4a39-90d5-83d3d881f0ed"),
                 UUID.fromString("62c5850e-005f-47ae-8f0f-559df09fe681")
         });
-        put("충북", new UUID[] {
+        companyManagers.put(CHUNGBUK, new UUID[] {
                 UUID.fromString("d5f4e84b-84c8-43d2-b2b7-27d883d7b17d"),
                 UUID.fromString("48b50467-1bb4-42d0-a653-e753239210a3")
         });
-    }};
-
+    }
 
     // 허브별로 과일가게와 핸드폰 회사 생성
     private void createCompanies() {
-        String[] hubRegions = getHubRegions();
-
-        for (int i = 0; i < hubRegions.length; i++) {
-            createFruitShopCompany(hubRegions[i], hubIds[i], companyManagers.get(hubRegions[i])[0]);
-            createPhoneCompany(hubRegions[i], hubIds[i], companyManagers.get(hubRegions[i])[1]);
+        for (String region : hubMap.keySet()) {
+            createFruitShopCompany(region, hubMap.get(region), companyManagers.get(region)[0]);
+            createPhoneCompany(region, hubMap.get(region), companyManagers.get(region)[1]);
         }
     }
 
---
+++ BOOT/hub/src/main/java/com/coopang/hub/infrastructure/configuration/ShipperDataInitializer.java
@@ -0,0 +1,123 @@
+package com.coopang.hub.infrastructure.configuration;
+
+import static com.coopang.apiconfig.initdata.HubRegions.BUSAN;
+import static com.coopang.apiconfig.initdata.HubRegions.CHUNGBUK;
+import static com.coopang.apiconfig.initdata.HubRegions.DAEGU;
+import static com.coopang.apiconfig.initdata.HubRegions.DAEJEON;
+import static com.coopang.apiconfig.initdata.HubRegions.GANGWON;
+import static com.coopang.apiconfig.initdata.HubRegions.GWANGJU;
+import static com.coopang.apiconfig.initdata.HubRegions.GYEONGGI_NORTH;
+import static com.coopang.apiconfig.initdata.HubRegions.GYEONGGI_SOUTH;
+import static com.coopang.apiconfig.initdata.HubRegions.INCHEON;
+import static com.coopang.apiconfig.initdata.HubRegions.SEJONG;
+import static com.coopang.apiconfig.initdata.HubRegions.SEOUL;
+import static com.coopang.apiconfig.initdata.HubRegions.ULSAN;
+
+import com.coopang.apiconfig.initdata.HubMapInitializer;
+import com.coopang.apidata.application.hub.enums.ShipperTypeEnum;
+import com.coopang.hub.application.request.shipper.ShipperDto;
+import com.coopang.hub.application.service.shipper.ShipperService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+@Component
+public class ShipperDataInitializer implements CommandLineRunner {
+    private final ShipperService shipperService;
+    private Map<String, UUID> hubMap;
+
+    public ShipperDataInitializer(ShipperService shipperService, HubMapInitializer hubMapInitializer) {
+        this.shipperService = shipperService;
+        this.hubMap = hubMapInitializer.getHubMap();
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        createShippers();
+    }
+
+    // hubId에 매핑되는 지역 데이터
+    List<ShipperDto> shipperDtoList = List.of(
+            // 서울
+            createShipperDto("3a1386db-8ad3-4cac-a234-31925ada139b", SEOUL, "ShipperHub-서울"),
+            createShipperDto("ec397ca8-dfd0-4b94-912c-233265282316", SEOUL, "ShipperCustomer 1 - 서울"),
+            createShipperDto("30ecd2e9-8dee-4478-b418-a7a293805772", SEOUL, "ShipperCustomer 2 - 서울"),
+
+            // 경기북부
+            createShipperDto("d8d5414e-7812-4b01-99e2-1055cbccf676", GYEONGGI_NORTH, "ShipperHub-경기북부"),
+            createShipperDto("0cfeddd6-e080-4a1f-b918-b80e0064cf8c", GYEONGGI_NORTH, "ShipperCustomer 1 - 경기북부"),
+            createShipperDto("bb659b6b-da76-4cd9-ae54-e3d5dc671910", GYEONGGI_NORTH, "ShipperCustomer 2 - 경기북부"),
+
+            // 경기남부
+            createShipperDto("6b4f5a31-5345-45c7-a167-dd1fd2cd6ae8", GYEONGGI_SOUTH, "ShipperHub-경기남부"),
+            createShipperDto("fdb75eed-390a-4aad-8bed-611829fba713", GYEONGGI_SOUTH, "ShipperCustomer 1 - 경기남부"),
+            createShipperDto("5800e01b-5e60-4261-ade6-20953f7e9d24", GYEONGGI_SOUTH, "ShipperCustomer 2 - 경기남부"),
+
+            // 부산
+            createShipperDto("58e95489-8a76-4e46-84b5-ac7ce78e35ca", BUSAN, "ShipperHub-부산"),
+            createShipperDto("48410e1f-69c2-41d6-bd6e-892329bff329", BUSAN, "ShipperCustomer 1 - 부산"),
+            createShipperDto("ad1ee1d0-53fd-495f-9f35-2cdb2aa6be05", BUSAN, "ShipperCustomer 2 - 부산"),
+
+            // 대구
+            createShipperDto("6a26ed86-79f6-46c8-a830-239e9bd45350", DAEGU, "ShipperHub-대구"),
+            createShipperDto("fd96ab07-3350-4298-8570-34ca189315ee", DAEGU, "ShipperCustomer 1 - 대구"),
+            createShipperDto("afa5f951-02eb-4442-9fa1-363bc8dcec26", DAEGU, "ShipperCustomer 2 - 대구"),
+
+            // 인천
+            createShipperDto("0c433c56-4c5d-45f2-989a-8db15f55226a", INCHEON, "ShipperHub-인천"),
+            createShipperDto("2e418446-0e43-4af9-b34a-beb1470a87cb", INCHEON, "ShipperCustomer 1 - 인천"),
+            createShipperDto("9440bc94-26cb-4a7a-8668-371f19ace9dc", INCHEON, "ShipperCustomer 2 - 인천"),
+
+            // 광주
+            createShipperDto("bd1fa464-5b68-4d09-977c-015f3e9c2f6a", GWANGJU, "ShipperHub-광주"),
+            createShipperDto("b4c8a76c-df35-4066-8203-3df2455fcf88", GWANGJU, "ShipperCustomer 1 - 광주"),
+            createShipperDto("99f26f79-1ed5-4799-b7a0-058a00df55eb", GWANGJU, "ShipperCustomer 2 - 광주"),
+
+            // 대전
+            createShipperDto("37d3078c-2a9a-485d-8a2f-09f5c6874e94", DAEJEON, "ShipperHub-대전"),
+            createShipperDto("06abaa95-2431-4ff5-8472-5bc17a16a5c3", DAEJEON, "ShipperCustomer 1 - 대전"),
+            createShipperDto("b320b1f4-197a-41de-bb68-b882fc0b2870", DAEJEON, "ShipperCustomer 2 - 대전"),
+
+            // 울산
+            createShipperDto("531de8b3-2008-4f13-9511-7ee406c2e14a", ULSAN, "ShipperHub-울산"),
+            createShipperDto("afa88bdc-0633-4c01-a220-5308ccb5e765", ULSAN, "ShipperCustomer 1 - 울산"),
+            createShipperDto("0d8ca948-6881-4dd6-a4a9-79ebe2692d8d", ULSAN, "ShipperCustomer 2 - 울산"),
+
+            // 세종
+            createShipperDto("d2bcfe27-a27c-4efb-978a-22c5c674929c", SEJONG, "ShipperHub-세종"),
+            createShipperDto("0f343a78-e7a9-47f1-a669-bb301acbc7db", SEJONG, "ShipperCustomer 1 - 세종"),
+            createShipperDto("4b933781-8b93-4ebf-bfd1-4ca447cd9416", SEJONG, "ShipperCustomer 2 - 세종"),
+
+            // 강원
+            createShipperDto("1d954968-9cad-4526-a1df-5cce4aace3ae", GANGWON, "ShipperHub-강원"),
+            createShipperDto("ef88eab0-66a3-43fd-894b-5e3d1bbfc348", GANGWON, "ShipperCustomer 1 - 강원"),
+            createShipperDto("eb9169b3-b3d8-4f9b-8e31-95706711fd41", GANGWON, "ShipperCustomer 2 - 강원"),
+
+            // 충북
+            createShipperDto("bbb4b3d5-ccfe-467d-a7a5-b5e8a711230a", CHUNGBUK, "ShipperHub-충북"),
+            createShipperDto("cf004528-251f-4ced-89f1-2cb6d81c9b58", CHUNGBUK, "ShipperCustomer 1 - 충북"),
+            createShipperDto("53e4ad5c-0a53-446b-b31a-11416a4d364e", CHUNGBUK, "ShipperCustomer 2 - 충북")
+    );
+
+    private void createShippers() {
+        for (ShipperDto shipperDto : shipperDtoList) {
+            shipperService.createShipper(shipperDto);
+        }
+    }
+
+    private ShipperDto createShipperDto(String shipperId, String region, String userName) {
+        ShipperTypeEnum shipperType = userName.contains("ShipperHub")
+                ? ShipperTypeEnum.SHIPPER_HUB
+                : ShipperTypeEnum.SHIPPER_CUSTOMER;
+
+        ShipperDto shipperDto = new ShipperDto();
+        shipperDto.setShipperId(UUID.fromString(shipperId));
+        shipperDto.setHubId(hubMap.get(region));
+        shipperDto.setShipperType(shipperType);
+
+        return shipperDto;
+    }
+}
