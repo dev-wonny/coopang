431b470606ef9379d3aa66fb11d0cf821748ff2a
“devwonny” <devwonny@gmail.com>
2024-10-11 17:34:36 +0900

    [Add] api-data: for Adjusting FeignClient for Common Module(api-communication)
    [Add] api-data: for Adjusting FeignClient for Common Module(api-communication)
    
    - [Add] CompanySearchConditionRequest, CompanyResponse, HubSearchConditionRequest, ShipperSearchConditionRequest, ShipperResponse, UserSearchConditionRequest
    
    - [Update] HubResponse, UserResponse : Add AllArgsConstructor
    
    - [Update] UserConstants : Add HEADER_USER_ID, HEADER_USER_ROLE for gateway-app

---
+++ DATA/api-data/src/main/java/com/coopang/apidata/application/user/request/UserSearchConditionRequest.java
@@ -0,0 +1,16 @@
+package com.coopang.apidata.application.user.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+public class UserSearchConditionRequest {
+    private UUID userId;
+    private String userName;
+    private String userRole;
+    private String email;
+    private Boolean isDeleted;
+}
