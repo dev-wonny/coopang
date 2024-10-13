965faedae14ff2da3850dce60660f846111d98b8
“devwonny” <devwonny@gmail.com>
2024-10-13 11:49:26 +0900

    [Update] product-app : service 정리 가이드
    [Update] product-app : service 정리
    - [Add] ProductKafkaService : ProductKafkaService 서비스로 분리
    - [Add] ProductProductStockService : 트랜잭션을 위해서 공통 서비스 생성
    - [Update] ProductController : ProductProductStockService 생성으로 수정, HEADER_USER_ID, HEADER_USER_ROLE 변경 from apiconfig.

---
+++ BOOT/product/src/main/java/com/coopang/product/application/service/productstock/ProductStockService.java
@@ -0,0 +1,11 @@
+package com.coopang.product.application.service.productstock;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductStockService {
+    public int createProductStock(int productStock) {
+
+        return 1;
+    }
+}
--- BOOT/product/src/main/java/com/coopang/product/presentation/controller/product/ProductController.java
+++ BOOT/product/src/main/java/com/coopang/product/presentation/controller/product/ProductController.java
@@ -1,11 +1,14 @@
 package com.coopang.product.presentation.controller.product;
 
+import static com.coopang.apiconfig.constants.HeaderConstants.HEADER_USER_ID;
+import static com.coopang.apiconfig.constants.HeaderConstants.HEADER_USER_ROLE;
+
 import com.coopang.apiconfig.mapper.ModelMapperConfig;
-import com.coopang.apidata.application.user.enums.UserRoleEnum;
 import com.coopang.apidata.application.user.enums.UserRoleEnum.Authority;
 import com.coopang.product.application.request.product.ProductDto;
 import com.coopang.product.application.request.product.ProductHiddenAndSaleDto;
-import com.coopang.product.application.service.product.ProductService;
+import com.coopang.product.application.response.product.ProductResponseDto;
+import com.coopang.product.application.service.ProductProductStockService;
 import com.coopang.product.presentation.request.product.BaseSearchCondition;
 import com.coopang.product.presentation.request.product.CreateProductRequestDto;
 import com.coopang.product.presentation.request.product.ProductSearchCondition;
@@ -14,7 +17,6 @@ import com.coopang.product.presentation.request.product.UpdateProductRequest;
 import com.coopang.product.presentation.request.product.UpdateProductSaleRequest;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -32,6 +34,8 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 
 @Tag(name = "ProductController API", description = "ProductController API")
 @RestController
@@ -40,60 +44,57 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
     private final ModelMapperConfig mapperConfig;
-    private final ProductService productService;
 
-    private static final String USER_ID_HEADER = "X-User-Id";
-    private static final String USER_ROLE_HEADER = "X-User-Role";
+    private final ProductProductStockService productStockService;
 
-    public ProductController(ModelMapperConfig mapperConfig,ProductService productService) {
+    public ProductController(ModelMapperConfig mapperConfig, ProductProductStockService productStockService) {
         this.mapperConfig = mapperConfig;
-        this.productService = productService;
+        this.productStockService = productStockService;
     }
 
-    @Secured({Authority.MASTER,Authority.COMPANY,Authority.HUB_MANAGER})
+    @Secured({Authority.MASTER, Authority.COMPANY, Authority.HUB_MANAGER})
     @PostMapping("/product")
-    public ResponseEntity<?> createProduct(@Valid @RequestBody CreateProductRequestDto createProductRequestDto) {
-
-        ProductDto productDto = mapperConfig.strictMapper().map(createProductRequestDto, ProductDto.class);
-
-        return new ResponseEntity<>(productService.createProduct(productDto),HttpStatus.CREATED);
+    public ResponseEntity<ProductResponseDto> createProduct(@Valid @RequestBody CreateProductRequestDto createProductRequestDto) {
+        final ProductDto productDto = mapperConfig.strictMapper().map(createProductRequestDto, ProductDto.class);
+        final ProductResponseDto productResponseDto = productStockService.createProductAndProductStock(productDto);
+        return new ResponseEntity<>(productResponseDto, HttpStatus.CREATED);
     }
 
-    @Secured({Authority.MASTER,Authority.COMPANY,Authority.HUB_MANAGER})
+    @Secured({Authority.MASTER, Authority.COMPANY, Authority.HUB_MANAGER})
     @PutMapping("/product/{productId}")
-    public ResponseEntity<?> updateProduct(@Valid @RequestBody UpdateProductRequest updateProductRequest,@PathVariable UUID productId) {
+    public ResponseEntity<?> updateProduct(@Valid @RequestBody UpdateProductRequest updateProductRequest, @PathVariable UUID productId) {
 
         ProductDto productDto = mapperConfig.strictMapper().map(updateProductRequest, ProductDto.class);
-        productService.updateProduct(productDto,productId);
+        productService.updateProduct(productDto, productId);
 
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @Secured({Authority.MASTER,Authority.COMPANY,Authority.HUB_MANAGER})
+    @Secured({Authority.MASTER, Authority.COMPANY, Authority.HUB_MANAGER})
     @PatchMapping("/product/{productId}/hidden")
-    public ResponseEntity<?> updateProductHidden(@Valid @RequestBody UpdateProductHiddenRequest request,@PathVariable UUID productId) {
+    public ResponseEntity<?> updateProductHidden(@Valid @RequestBody UpdateProductHiddenRequest request, @PathVariable UUID productId) {
 
         ProductHiddenAndSaleDto productHiddenAndSaleDto = mapperConfig.strictMapper().map(request, ProductHiddenAndSaleDto.class);
-        productService.updateProductHidden(productHiddenAndSaleDto,productId);
+        productService.updateProductHidden(productHiddenAndSaleDto, productId);
 
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @Secured({Authority.MASTER,Authority.COMPANY,Authority.HUB_MANAGER})
+    @Secured({Authority.MASTER, Authority.COMPANY, Authority.HUB_MANAGER})
     @PatchMapping("/product/{productId}/sale")
     public ResponseEntity<?> updateProductSale(@Valid @RequestBody UpdateProductSaleRequest request, @PathVariable UUID productId) {
 
         ProductHiddenAndSaleDto productHiddenAndSaleDto = mapperConfig.strictMapper().map(request, ProductHiddenAndSaleDto.class);
-        productService.updateProductSale(productHiddenAndSaleDto,productId);
+        productService.updateProductSale(productHiddenAndSaleDto, productId);
 
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @Secured({Authority.MASTER,Authority.COMPANY,Authority.HUB_MANAGER})
+    @Secured({Authority.MASTER, Authority.COMPANY, Authority.HUB_MANAGER})
     @DeleteMapping("/product/{productId}")
     public ResponseEntity<?> deleteProduct(@PathVariable UUID productId,
-        @RequestHeader(USER_ID_HEADER) UUID userId,
-        @RequestHeader(USER_ROLE_HEADER) String role){
+                                           @RequestHeader(HEADER_USER_ID) UUID userId,
+                                           @RequestHeader(HEADER_USER_ROLE) String role) {
 
         productService.deleteProductById(userId, role, productId);
 
@@ -103,26 +104,26 @@ public class ProductController {
 
     @GetMapping("/product")
     public ResponseEntity<?> getAllProducts(@ModelAttribute BaseSearchCondition condition,
-        @RequestHeader(USER_ROLE_HEADER) String role,Pageable pageable) {
+                                            @RequestHeader(HEADER_USER_ROLE) String role, Pageable pageable) {
 
-        return new ResponseEntity<>(productService.getAllProducts(condition,role,pageable),HttpStatus.OK);
+        return new ResponseEntity<>(productService.getAllProducts(condition, role, pageable), HttpStatus.OK);
     }
 
     @GetMapping("/product/search")
-    public ResponseEntity<?> searchProduct(@ModelAttribute ProductSearchCondition searchCondition,Pageable pageable) {
+    public ResponseEntity<?> searchProduct(@ModelAttribute ProductSearchCondition searchCondition, Pageable pageable) {
 
-        return new ResponseEntity<>(productService.searchProduct(searchCondition,pageable),HttpStatus.OK);
+        return new ResponseEntity<>(productService.searchProduct(searchCondition, pageable), HttpStatus.OK);
     }
 
     @GetMapping("/product/{productId}")
     public ResponseEntity<?> getProductById(@PathVariable UUID productId) {
 
-        return new ResponseEntity<>(productService.getProductById(productId),HttpStatus.OK);
+        return new ResponseEntity<>(productService.getProductById(productId), HttpStatus.OK);
     }
 
     @GetMapping("/category/{categoryId}/product")
-    public ResponseEntity<?> getProductWithCategory(@PathVariable UUID categoryId,Pageable pageable) {
+    public ResponseEntity<?> getProductWithCategory(@PathVariable UUID categoryId, Pageable pageable) {
 
-        return new ResponseEntity<>(productService.getProductWithCategory(categoryId,pageable),HttpStatus.OK);
+        return new ResponseEntity<>(productService.getProductWithCategory(categoryId, pageable), HttpStatus.OK);
     }
 }
