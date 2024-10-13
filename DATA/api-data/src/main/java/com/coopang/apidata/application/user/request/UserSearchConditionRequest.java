c986f24d819f285412f50143ec152db062226d47
“devwonny” <devwonny@gmail.com>
2024-10-13 16:11:40 +0900

    [Update] apidata-app : @Email 추가
    [Update] apidata-app
    - [Update] UserSearchConditionRequest : @Email 추가

--- DATA/api-data/src/main/java/com/coopang/apidata/application/user/request/UserSearchConditionRequest.java
+++ DATA/api-data/src/main/java/com/coopang/apidata/application/user/request/UserSearchConditionRequest.java
@@ -1,5 +1,6 @@
 package com.coopang.apidata.application.user.request;
 
+import jakarta.validation.constraints.Email;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,6 +12,7 @@ public class UserSearchConditionRequest {
     private UUID userId;
     private String userName;
     private String userRole;
+    @Email(message = "Email should be valid")
     private String email;
     private Boolean isDeleted;
 }
