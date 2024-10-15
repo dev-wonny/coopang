5a5821e1f71d2b7d977d244250bdb8e70eb56791
“devwonny” <devwonny@gmail.com>
2024-10-15 18:55:20 +0900

    [Init] DATA:core-data
    [Init] DATA:core-data
    - [Add] HeaderConstants
    - [Add] UserRoleEnum from DATA:api-data

---
+++ DATA/core-data/src/main/java/com/coopang/coredata/user/constants/HeaderConstants.java
@@ -0,0 +1,7 @@
+package com.coopang.coredata.user.constants;
+
+public class HeaderConstants {
+    public static final String HEADER_USER_ID = "X-User-Id";
+    public static final String HEADER_USER_ROLE = "X-User-Role";
+    public static final String HEADER_TOKEN = "X-Token";
+}
