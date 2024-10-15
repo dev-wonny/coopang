086a72d5c8a4bb393a0ea6d625662d80ba52c9e9
“devwonny” <devwonny@gmail.com>
2024-10-15 18:55:20 +0900

    [Init] DATA:core-data
    [Init] DATA:core-data
    - [Add] HeaderConstants
    - [Add] UserRoleEnum from DATA:api-data

---
+++ DATA/core-data/src/test/java/com/coopang/coredata/CoreDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coopang.coredata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
