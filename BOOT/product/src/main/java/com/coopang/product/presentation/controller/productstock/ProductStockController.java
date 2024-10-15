485ebfa4287ba957777af3a7f0cae48f3545f436
bongdaehyun <44823780+bongdaehyun@users.noreply.github.com>
2024-10-15 12:06:09 +0900

    [Refactor] delete generic
    [Refactor] delete generic

--- BOOT/product/src/main/java/com/coopang/product/presentation/controller/productstock/ProductStockController.java
+++ BOOT/product/src/main/java/com/coopang/product/presentation/controller/productstock/ProductStockController.java
@@ -33,7 +33,7 @@ public class ProductStockController {
 
     @Secured({Authority.MASTER, Authority.COMPANY, Authority.HUB_MANAGER})
     @PatchMapping("/{productId}/restock")
-    public ResponseEntity<String> addProductStock(@PathVariable UUID productId,
+    public ResponseEntity<Void> addProductStock(@PathVariable UUID productId,
                                                   @Valid @RequestBody AddStockRequestDto addStockRequestDto) {
 
         ProductStockDto productStockDto = mapperConfig.strictMapper().map(addStockRequestDto, ProductStockDto.class);
@@ -45,7 +45,7 @@ public class ProductStockController {
 
     @Secured({Authority.MASTER, Authority.COMPANY, Authority.HUB_MANAGER})
     @PatchMapping("/{productId}/reduce")
-    public ResponseEntity<String> reduceProductStock(
+    public ResponseEntity<Void> reduceProductStock(
             @PathVariable UUID productId,
             @Valid @RequestBody UpdateStockRequestDto updateStockRequestDto) {
 
@@ -58,7 +58,7 @@ public class ProductStockController {
 
     @Secured({Authority.MASTER, Authority.COMPANY, Authority.HUB_MANAGER})
     @PatchMapping("/{productId}/rollback-increase")
-    public ResponseEntity<String> rollbackProductStock(
+    public ResponseEntity<Void> rollbackProductStock(
             @PathVariable UUID productId,
             @Valid @RequestBody UpdateStockRequestDto updateStockRequestDto) {
 
@@ -71,7 +71,7 @@ public class ProductStockController {
 
     @Secured({Authority.MASTER, Authority.COMPANY, Authority.HUB_MANAGER})
     @PatchMapping("/{productId}/cancel-increase")
-    public ResponseEntity<String> cancelProductStock(
+    public ResponseEntity<Void> cancelProductStock(
             @PathVariable UUID productId,
             @Valid @RequestBody UpdateStockRequestDto updateStockRequestDto) {
 
