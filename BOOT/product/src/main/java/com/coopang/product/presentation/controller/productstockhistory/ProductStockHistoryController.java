154f6f9da90ce31f082c6205f33c87ed21ab8850
bongdaehyun <44823780+bongdaehyun@users.noreply.github.com>
2024-10-17 14:39:30 +0900

    [Refactor] 불필요한 import 삭제
    [Refactor] 불필요한 import 삭제

--- BOOT/product/src/main/java/com/coopang/product/presentation/controller/productstockhistory/ProductStockHistoryController.java
+++ BOOT/product/src/main/java/com/coopang/product/presentation/controller/productstockhistory/ProductStockHistoryController.java
@@ -18,8 +18,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
