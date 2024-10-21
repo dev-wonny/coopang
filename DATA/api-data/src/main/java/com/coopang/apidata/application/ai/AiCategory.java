ea1a882c2e61f891a385fbc33b2d9753bc177df3
“devwonny” <115609633+wonnyhan@users.noreply.github.com>
2024-10-21 20:17:16 +0900

    [Update] DATA:api-data
    [Update] DATA:api-data
    - [Update] AiCategory : Add getAiCategoryEnum()
    - [Update] ShipperTypeEnum : change method name getRoleEnum -> getShipperTypeEnum

--- DATA/api-data/src/main/java/com/coopang/apidata/application/ai/AiCategory.java
+++ DATA/api-data/src/main/java/com/coopang/apidata/application/ai/AiCategory.java
@@ -4,4 +4,13 @@ public enum AiCategory {
     TODAY_WEATHER
     , TODAY_SHIPPER_SUMMARY_DELIVERY
     , TODAY_HUB_SUMMARY_ORDER
+    ;
+
+    public static AiCategory getAiCategoryEnum(String s) {
+        try {
+            return AiCategory.valueOf(s);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Invalid Ai Category: " + s);
+        }
+    }
 }
