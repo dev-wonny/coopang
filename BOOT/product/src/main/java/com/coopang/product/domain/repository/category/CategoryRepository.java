597121ebf6abec7a748db19e1b898f0d01f7f67f
MinChur <145955453+jiminchur@users.noreply.github.com>
2024-10-23 00:53:45 +0900

    [fix] conflict 해결하기

--- BOOT/product/src/main/java/com/coopang/product/domain/repository/category/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.coopang.product.domain.repository.category;
-
-import com.coopang.product.domain.entity.category.CategoryEntity;
-import java.util.Optional;
-import java.util.UUID;
-
-public interface CategoryRepository {
-
-}
