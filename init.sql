c16352a412413047a79570af68d7088ad143cdcc
MinChur <145955453+jiminchur@users.noreply.github.com>
2024-10-23 00:49:34 +0900

    [fix] conflict 해결하기

--- init.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE coopang_user;
-CREATE DATABASE coopang_hub;
-CREATE DATABASE coopang_company;
-CREATE DATABASE coopang_order;
-CREATE DATABASE coopang_delivery;
-CREATE DATABASE coopang_ainoti;
